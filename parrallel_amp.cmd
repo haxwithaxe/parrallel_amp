0ba4c9dae2fb143c3c411912483716b3e6ba5466
Chris Koepke <me@haxwithaxe.net>
2013-08-28 14:36:42 -0400

    adding everything else

---
+++ parrallel_amp.cmd
@@ -0,0 +1,58 @@
+# Pin name action command file
+
+# Start of element R_1
+ChangePinName(R_1, 2, 2)
+ChangePinName(R_1, 1, 1)
+
+# Start of element PNP_1
+ChangePinName(PNP_1, 2, B)
+ChangePinName(PNP_1, 3, E)
+ChangePinName(PNP_1, 1, C)
+
+# Start of element NPN_1
+ChangePinName(NPN_1, 2, B)
+ChangePinName(NPN_1, 3, E)
+ChangePinName(NPN_1, 1, C)
+
+# Start of element OPA_1
+ChangePinName(OPA_1, 7, V+)
+ChangePinName(OPA_1, 6, out)
+ChangePinName(OPA_1, 5, offset null)
+ChangePinName(OPA_1, 4, V-)
+ChangePinName(OPA_1, 3, +)
+ChangePinName(OPA_1, 2, -)
+ChangePinName(OPA_1, 1, offset null)
+
+# Start of element R_102
+ChangePinName(R_102, 2, 2)
+ChangePinName(R_102, 1, 1)
+
+# Start of element Q?
+ChangePinName(Q?, 2, B)
+ChangePinName(Q?, 3, E)
+ChangePinName(Q?, 1, C)
+
+# Start of element NPN_102
+ChangePinName(NPN_102, 2, B)
+ChangePinName(NPN_102, 3, E)
+ChangePinName(NPN_102, 1, C)
+
+# Start of element OPA_102
+ChangePinName(OPA_102, 7, V+)
+ChangePinName(OPA_102, 6, out)
+ChangePinName(OPA_102, 5, offset null)
+ChangePinName(OPA_102, 4, V-)
+ChangePinName(OPA_102, 3, +)
+ChangePinName(OPA_102, 2, -)
+ChangePinName(OPA_102, 1, offset null)
+
+# Start of element power101
+ChangePinName(power101, 3, 3)
+ChangePinName(power101, 1, 1)
+ChangePinName(power101, 2, 2)
+
+# Start of element CONN?
+ChangePinName(CONN?, 1, 1)
+ChangePinName(CONN?, 2, 2)
+ChangePinName(CONN?, 1, 1)
+ChangePinName(CONN?, 2, 2)
